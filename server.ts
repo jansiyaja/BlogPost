7bfa7a7e502fde26a7dc66ceca69d7cb06979750
Jansiya jahan <jansiyajahan8@gmail.com>
2024-09-04 10:18:01 +0530

    register  issues fixed

--- server.ts
+++ server.ts
@@ -1,15 +1,17 @@
-
 import express from 'express';
 import { connectDb } from './backend/framework/config/db';
 import dotenv from 'dotenv';
-
+import userRoutes from './backend/Router/userRoutes';
+import cors from 'cors';
 
 const app = express();
-dotenv.config()
-
-
+dotenv.config();
 
 connectDb();
+app.use(cors()); 
+
+app.use(express.json());  
+app.use('/users', userRoutes);
 
 app.listen(3000, (): void => {
     console.log("Listening on port 3000");
